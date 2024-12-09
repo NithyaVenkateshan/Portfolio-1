a5f163eed18d4b3259c470a57dbd2424a54d47ce
Nithya <nithyavenkateshan18@gmail.com>
2024-12-09 00:03:28 -0800

    Delete port.js

--- port.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    // Hide all sections
-    document.querySelectorAll('section').forEach(section => {
-        section.style.display = 'none';
-    });
-
-    // Show the clicked section
-    document.getElementById(sectionId).style.display = 'block';
-}
